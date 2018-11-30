ed4f9d98c68369d07a86953395d75e0f6e26bc24
JesusCDev <jesuscdev@gmail.com>
2018-11-30 05:32:25 -0500

    Adding the position changer into the settings. Should delete all data if they turn it off or no. Need to think about it. Next will work on fixing the position changer. But other settings buttons are create now to just set values.

--- app/src/main/java/com/allvens/allworkouts/SettingsActivity.java
+++ app/src/main/java/com/allvens/allworkouts/SettingsActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.allvens.allworkouts.data_manager.database.Workout_Wrapper;
+import com.allvens.allworkouts.settings_manager.Settings_Manager;
 
 public class SettingsActivity extends AppCompatActivity{
 
@@ -13,13 +15,18 @@ public class SettingsActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivty_settings);
+
+        LinearLayout ll_settings_WorkoutPositions = findViewById(R.id.ll_settings_WorkoutPositions);
+
+        Settings_Manager manager = new Settings_Manager(this);
+
+        manager.setUp_WorkoutsAndPositions(ll_settings_WorkoutPositions);
     }
 
     public void btnAction_ResetAlWorkouts(View view){
         Workout_Wrapper wrapper = new Workout_Wrapper(this);
         wrapper.open();
         wrapper.delete_AllWorkouts();
-        wrapper.delete_AllHistoryWorkouts();
         wrapper.close();
     }
 }
